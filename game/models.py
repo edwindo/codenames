29cc5330d2970c3e8f0d1e61424d04298f852d5b
Edwin Do <Edwin@Edwins-MacBook-Air.local>
2016-09-06 18:28:39 -0700

    added Card model

--- game/models.py
+++ game/models.py
@@ -2,4 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Card(models.Model):
+	word = models.CharField(max_length = 50)
+	color = models.CharField(max_length = 10)
+	visibility = models.BooleanField()
+
